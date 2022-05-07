86d271733a3e816361e811414686aaf8c9e176ee
Mofajjal Rasul <imzihad@gmail.com>
2022-05-07 15:47:16 +0600

    Initialized eslint-airbnb-prettier with typescript
    Signed-off-by: Mofajjal Rasul <imzihad@gmail.com>

---
+++ eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "jest"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "linebreak-style": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
