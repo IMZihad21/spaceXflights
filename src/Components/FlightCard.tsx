0902c14eac9579ca2c4dca8e7789d7f5e62f9f14
ZèD <imzihad@gmail.com>
2022-05-09 11:39:04 +0600

    Added more details in flight cards
    Signed-off-by: ZèD <imzihad@gmail.com>

--- src/Components/FlightCard.tsx
+++ src/Components/FlightCard.tsx
@@ -1,13 +1,16 @@
-import * as React from "react";
+import React from "react";
 import {
   Card,
   CardContent,
-  CardMedia,
+  Box,
   Typography,
   Button,
   CardActions,
+  Link,
+  Divider,
 } from "@mui/material";
 import { FlightType } from "Interfaces/FlightType";
+import { Link as RouterLink } from "react-router-dom";
 
 interface FlightCardProps {
   flight: FlightType;
@@ -24,31 +27,169 @@ const FlightCard: React.FC<FlightCardProps> = ({ flight }) => {
         flexDirection: "column",
       }}
     >
-      <CardMedia
-        component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
-      />
       <CardContent
         sx={{
           flexGrow: 1,
+          display: "flex",
+          justifyContent: "space-between",
+          gap: 1,
         }}
       >
-        <Typography gutterBottom variant="h5" component="div">
-          {flight.mission_name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {flight.details
-            ? flight.details?.length > 100
-              ? flight.details.substring(0, 90) + " ..."
-              : flight.details
-            : "No details available"}
-        </Typography>
+        <Box>
+          <Box
+            component="div"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h5">{flight.mission_name}</Typography>
+            {flight.upcoming && (
+              <Typography
+                variant="body2"
+                component="span"
+                sx={{
+                  bgcolor: "#707070",
+                  color: "#fff",
+                  padding: "3px",
+                  mx: "5px",
+                  borderRadius: "5px",
+                }}
+              >
+                Upcoming
+              </Typography>
+            )}
+          </Box>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+            color="textSecondary"
+            component="div"
+            fontWeight="bold"
+          >
+            {flight.rocket.rocket_name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {flight.details
+              ? flight.details?.length > 80
+                ? flight.details.substring(0, 80) + " ..."
+                : flight.details
+              : "No details available"}
+          </Typography>
+        </Box>
+        <Box
+          component="img"
+          src={flight.links?.mission_patch_small}
+          sx={{
+            height: "100%",
+            width: "120px",
+          }}
+        />
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+      <Divider />
+      <CardContent>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="body2" color="text.secondary">
+            Launch date:
+          </Typography>
+          <Typography variant="body2">
+            {new Date(flight.launch_date_utc).toLocaleString("en-US", {
+              month: "short",
+              day: "numeric",
+              year: "numeric",
+              hour: "numeric",
+              minute: "numeric",
+              timeZone: "Asia/Dhaka",
+            })}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="body2" color="text.secondary">
+            Launch status:
+          </Typography>
+          <Typography variant="body2">
+            {flight.launch_success === null
+              ? "Unknown"
+              : flight.launch_success
+              ? "Success"
+              : "Failed"}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="body2" color="text.secondary">
+            Launch site:
+          </Typography>
+          <Typography variant="body2">
+            {flight.launch_site?.site_name || "Unknown"}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="body2" color="text.secondary">
+            Rocket Type:
+          </Typography>
+          <Typography variant="body2">{flight.rocket.rocket_type}</Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="body2" color="text.secondary">
+            Nationality:
+          </Typography>
+          <Typography variant="body2"></Typography>
+        </Box>
+      </CardContent>
+      <CardActions sx={{ gap: "5px" }}>
+        <Button
+          component={Link}
+          size="small"
+          target="_blank"
+          href={`https://twitter.com/intent/tweet?text=${flight.mission_name}&url=${flight.links?.article_link}`}
+        >
+          Share
+        </Button>
+        <Button
+          component={Link}
+          size="small"
+          target="_blank"
+          href={flight.links?.article_link}
+        >
+          Read Article
+        </Button>
+        <Button
+          component={RouterLink}
+          size="small"
+          to={`/flight/${flight.flight_number}`}
+        >
+          Learn More
+        </Button>
       </CardActions>
     </Card>
   );
